df21326d88e8e6cbe2671b114fe95a50ef8483d8
Enes Kutlay <contact@eneskutlay.dev>
2022-09-26 05:39:04 +0300

    Manual Link component for active class

---
+++ components/ActiveLink.js
@@ -0,0 +1,25 @@
+import { useRouter } from "next/router";
+import PropTypes from "prop-types";
+import Link from "next/link";
+import React, { Children } from "react";
+
+const ActiveLink = ({ children, activeClassName, ...props }) => {
+  const { asPath } = useRouter();
+  const child = Children.only(children);
+  const childClassName = child.props.className || "";
+
+  const className =
+    asPath === props.href || asPath === props.as
+      ? `${childClassName} ${activeClassName}`.trim()
+      : childClassName;
+
+  return (
+    <Link {...props}>
+      {React.cloneElement(child, {
+        className: className || null,
+      })}
+    </Link>
+  );
+};
+
+export default ActiveLink;
