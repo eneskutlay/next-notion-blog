0d164e05c20d605f7b20f84d68d8a1f5a6597f14
Enes Kutlay <contact@eneskutlay.dev>
2022-09-26 05:40:02 +0300

    Navbar created

---
+++ components/Navbar.js
@@ -0,0 +1,20 @@
+import Link from "./ActiveLink";
+import styles from "./Navbar.module.css";
+
+export default function Navbar() {
+  return (
+    <div className={styles.navbar}>
+      <Link activeClassName={styles.activeLinks} href="/">
+        <a className={styles.home}>Home</a>
+      </Link>
+      <div className={styles.links}>
+        <Link activeClassName={styles.activeLinks} href="/writing">
+          <a className={styles.writing}>Writing</a>
+        </Link>
+        <Link activeClassName={styles.activeLinks} href="/about">
+          <a className={styles.about}>About</a>
+        </Link>
+      </div>
+    </div>
+  );
+}
