1d73857c2b4d1ceb861120594adaaff1a71cd816
Enes Kutlay <contact@eneskutlay.dev>
2022-09-26 21:38:17 +0300

    Card component created

---
+++ components/Card.js
@@ -0,0 +1,18 @@
+import styles from "./Card.module.css";
+import Image from "next/image";
+
+export default function Card(props) {
+  return (
+    <div className={styles.root}>
+      <a className={styles.item}>
+        <div className={styles.image}>
+          <Image src={"/github.png"} alt={props.title} width={64} height={64} />
+        </div>
+        <div className={styles.details}>
+          <h3 className={styles.detailsTitle}>{props.title}</h3>
+          <p className={styles.detailInfo}>{props.lang}</p>
+        </div>
+      </a>
+    </div>
+  );
+}
