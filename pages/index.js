c132bd934fe4d7f71f32f1a022bb198c5112f1cc
Enes Kutlay <contact@eneskutlay.dev>
2022-09-26 05:46:17 +0300

    Update Index.js

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,3 @@
-import styles from "../styles/Home.module.css";
-import Link from "next/link";
-
-
-export const databaseId = process.env.NOTION_DATABASE_ID;
-
 export default function Home() {
-  return (
-    <div>
-      <Link href="/writing">
-        <a>Blog</a>
-      </Link>
-    </div>
-  );
+  return <h1>Hello World</h1>;
 }
