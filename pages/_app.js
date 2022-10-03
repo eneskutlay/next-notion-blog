3fa64b114ed020b08fcf80ade0c78382eee0506f
Enes Kutlay <contact@eneskutlay.dev>
2022-10-03 03:25:06 +0300

    Deploy error fixed

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import "../styles/globals.css";
-import App from "next/app";
 import PageLayout from "../layout/PageLayout";
 
 function MyApp({ Component, pageProps }) {
@@ -9,16 +8,5 @@ function MyApp({ Component, pageProps }) {
     </PageLayout>
   );
 }
-
-// Only uncomment this method if you have blocking data requirements for
-// every single page in your application. This disables the ability to
-// perform automatic static optimization, causing every page in your app to
-// be server-side rendered.
-MyApp.getInitialProps = async (appContext) => {
-  // calls page's `getInitialProps` and fills `appProps.pageProps`
-  const appProps = await App.getInitialProps(appContext);
-
-  return { ...appProps };
-};
 
 export default MyApp;
--- pages/about.js
+++ pages/about.js
@@ -5,7 +5,7 @@ export default function About() {
   return (
     <div className={styles.container}>
       {about.map((item) => (
-        <div className={styles.aboutMeSections}>
+        <div key={item.chapter.indexOf} className={styles.aboutMeSections}>
           <p>{item.chapter}</p>
         </div>
       ))}
