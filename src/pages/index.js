d0e3fe30cda8b1636658ad4925e71fdc7d126adc
Enes Kutlay <hi@eneskutlay.dev>
2023-03-30 06:08:46 +0300

    🚀 Installed Next.js and Tailwind CSS.

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="flex max-w-2xl mx-auto bg-zinc-600">
+      <h1 className="text-6xl m-auto text-white">Hello World</h1>
+    </div>
+  );
+}
