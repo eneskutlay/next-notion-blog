3d5ff1512961557956da9a85b6dabc7e40f607a3
Enes Kutlay <hi@eneskutlay.dev>
2023-04-02 03:25:14 +0300

    Image alt element

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -8,6 +8,7 @@ function Bio() {
         className="mr-3 h-14 w-14 rounded-full"
         width={56}
         height={56}
+        alt="John Doe"
       />
       <p className=" max-w-xs p-2 text-base text-text">
         Personal blog by{" "}
