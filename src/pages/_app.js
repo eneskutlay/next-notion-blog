193b7230cf1803427d644782814f818fd940bfb1
Enes Kutlay <hi@eneskutlay.dev>
2023-04-05 03:49:36 +0300

    Vercel Analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,14 @@
 import "@/styles/globals.css";
 import Layout from "@/components/Layout";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      <Analytics />
+    </>
   );
 }
